f618cff20182b19f5ad8fc61a9bf0b689da321c9
leijin0416 <1427780404@qq.com>
2021-03-23 23:47:54 +0800

    表格组件

---
+++ src/components/ETable/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="table-box">
+    <el-table
+      ref="filterTable"
+      :data="tableData"
+      border
+      style="width: 100%"
+      @selection-change="handleSelectionChange" >
+      <template v-for="(column, index) in tableColumnData">
+        <!--复选框（START）-->
+        <el-table-column
+          v-if="column.type == 'selection'"
+          type="selection"
+          :width="column.width ? column.width : 60"
+          :align="column.align ? column.align : 'center'">
+        </el-table-column>
+        <!--序号（END）-->
+
+        <!-- 操作列/自定义列  slot 添加自定义配置项 -->
+        <slot v-else-if="column.slot" :name="column.slot" :tit='index'></slot>
+
+        <!-- 默认渲染列-渲染每一列的汉字 -->
+        <el-table-column
+          :prop="column.prop"
+          :label="column.label"
+          :align="column.align"
+          :width="column.width"
+          :show-overflow-tooltip="true"
+          v-else >
+        </el-table-column>
+
+      </template>
+    </el-table>
+    <div class="v-page-box">
+      <el-pagination
+        background
+        layout="total, prev, pager, next"
+        :total="totalCount"
+        :page-size="12"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange" >
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch, Emit } from 'vue-property-decorator';
+import { sessionData } from '@/filters/storage';
+
+@Component({
+  components: {},
+})
+export default class UserList extends Vue {
+
+  @Prop({ default: [] }) tableData!: Array<object>;   // 表格数据
+  @Prop({ default: [] }) tableColumnData!: Array<object>;  // 表格表头
+  @Prop({ default: '' }) totalCount!: number;  //  总信息条数
+
+
+  @Emit("handleSelectionChange")
+  private handleSelectionChange(val): void {
+    return val;
+  }
+
+  // 生命周期
+  mounted () {}
+
+  // 根据用户获取的每页显示页面数量显示页面
+  private handleSizeChange(val) {
+    console.log(val);
+  }
+
+  // 获取用户点击的当前页后刷新页面数据
+  @Emit('handleCurrentChange')
+  private handleCurrentChange(val) {
+    return val
+  }
+}
+</script>
+<style lang="scss" scoped>
+/deep/.el-table th {background: #f5f5f5;}
+.v-page-box {
+  padding: 30px 5px 5px;
+  text-align: right;
+}
+</style>
