882b4a1956ed400beecf09c2888faa74f9e44c75
leijin0416 <1427780404@qq.com>
2021-04-10 00:33:27 +0800

    提交代码

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ const cdn = {
     'https://cdn.bootcdn.net/ajax/libs/vue/2.6.9/vue.min.js',
     'https://cdn.bootcdn.net/ajax/libs/vue-router/3.2.0/vue-router.min.js',
     'https://cdn.bootcdn.net/ajax/libs/vuex/3.2.0/vuex.min.js',
-		'https://cdn.bootcdn.net/ajax/libs/axios/0.18.0/axios.min.js',
+		'https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js',
     "https://cdn.bootcdn.net/ajax/libs/vue-i18n/8.9.0/vue-i18n.min.js",
 		'https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.0/index.js',
     "https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.0/locale/en.min.js",
