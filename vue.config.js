923ebf1bace0768147a68e1c92d48555326e73ad
leijin0416 <1427780404@qq.com>
2021-10-20 00:45:01 +0800

    提交代码

--- vue.config.js
+++ vue.config.js
@@ -52,10 +52,10 @@ const cdn = {
 let baseUrl = "";   // 这里是一个默认的url，可以没有
 switch (isDev) {
   case 'development':
-    baseUrl = "http://192.168.1.xxxx:10086"  // 这里是本地的请求url
+    baseUrl = "http://192.168.1.xxxx"  // 这里是本地的请求url
     break
   case 'production':
-    baseUrl = "http://185.251.248.xxxx:10086"   // 生产环境url
+    baseUrl = "http://185.251.248.xxxx"   // 生产环境url
     break
 }
 let isDevCS = {
@@ -72,7 +72,6 @@ let isDevCS = {
 /**
  *  production：生产环境，development：开发环境
     config.mode = process.env.NODE_ENV
-    
     devServer: {
       open: false,
       https: false,
