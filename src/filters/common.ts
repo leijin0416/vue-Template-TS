b3e02943cb41db67a346a3971e81790f216ce185
leijin0416 <1427780404@qq.com>
2021-10-24 00:46:04 +0800

    常用工具库

--- src/filters/common.ts
+++ src/filters/common.ts
@@ -80,7 +80,8 @@
  * @param {*} S  秒
  * @returns 
  */
-const FTisFormatCurrentTime = (fmt, time) => {
+const FTisFormatCurrentTime = (fmt: string, time: string | number) => {
+  if (!time) return '';
   let date = new Date(time)
   let ret;
   let opt = {
@@ -99,6 +100,34 @@ const FTisFormatCurrentTime = (fmt, time) => {
     };
   };
   return fmt
+};
+
+/** 
+ * @description 时间戳换算日期
+ * @param {String} str  时间戳
+ * @returns 
+ */
+const FTisFormatTimeTextDate = (str: string | number) => {
+  if (!str) return '';
+  let date = new Date(Number(str));
+  let time = new Date().getTime() - date.getTime(); // 现在的时间-传入的时间 = 相差的时间（单位 = 毫秒）
+  if (time < 0) {
+    return '';
+  } else if (time / 1000 < 30) {
+    return '刚刚';
+  } else if (time / 1000 < 60) {
+    return parseInt(String(time / 1000)) + '秒前';
+  } else if (time / 60000 < 60) {
+    return parseInt(String(time / 60000)) + '分钟前';
+  } else if (time / 3600000 < 24) {
+    return parseInt(String(time / 3600000)) + '小时前';
+  } else if (time / 86400000 < 31) {
+    return parseInt(String(time / 86400000)) + '天前';
+  } else if (time / 2592000000 < 12) {
+    return parseInt(String(time / 2592000000)) + '月前';
+  } else {
+    return parseInt(String(time / 31536000000)) + '年前';
+  }
 };
 
 /** 
@@ -183,6 +212,7 @@ export {
   TreeForeach, 
   FormatArrMapHas, 
   FTisFormatCurrentTime,
+  FTisFormatTimeTextDate,
   FTisRoundDecimaleeForm, 
   FTisRemoveFormInput, 
   FTisRemoveFormInputTrim, 
