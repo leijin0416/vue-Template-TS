eecec99a202cf3021c8e69b3f588fe9f31571fed
leijin0416 <1427780404@qq.com>
2021-11-17 01:00:20 +0800

    按钮权限

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,10 @@ import i18n from './langs';
 import filters from './filters';
 
 import Viewer from 'v-viewer';
-import VueWechatTitle from 'vue-wechat-title';          // 动态修改
+import VueWechatTitle from 'vue-wechat-title';           // 动态修改
 import myECharts from "./components/Echarts/common";     // 挂载图表
-import './common/https/permission';                      // 全局按钮权限
+import './utils/btnPermission';                         // 全局按钮权限
 
-// import 'babel-polyfill'            // 用于实现浏览器不支持原生功能的代码
-// import ElementUI from 'element-ui'
-// import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
-
-// import 'element-ui/lib/theme-chalk/index.css'
 import './assets/styles/css/font.css';
 import './assets/styles/css/main.css';
 import './assets/styles/common.scss';
@@ -25,7 +20,6 @@ Vue.use(Viewer)
 Viewer.setDefaults({})
 Vue.use(VueWechatTitle)
 Vue.use(myECharts)
-// Vue.use(ElementUI)
 
 Vue.config.productionTip = false;
 
