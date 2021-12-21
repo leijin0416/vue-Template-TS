bb76c5522d81dec8e89a09eedcae984e3eb2b639
leijin0416 <1427780404@qq.com>
2021-12-22 00:55:44 +0800

    I18n 语言模块

--- src/langs/index.ts
+++ src/langs/index.ts
@@ -1,19 +1,19 @@
-import Vue from 'vue';
-import VueI18n, { LocaleMessages } from 'vue-i18n';
-import { sessionData } from '@/filters/storage';
+import Vue from 'vue'
+import VueI18n, { LocaleMessages } from 'vue-i18n'
+import { sessionData } from '@/filters/storage'
 
 // 方式二
-import zhLocaleI18n from './cn';
-import enLocaleI18n from './en';
+import zhLocaleI18n from './cn'
+import enLocaleI18n from './en'
 
 Vue.use(VueI18n)
 
 /**
  * @description: 语言包
  * @param {*} https://segmentfault.com/a/1190000020297356?utm_source=tag-newest TS中使用
- * @return {*}
+ * @return {*} {{ $t('m.message') }}
  */
-let messages = {
+const messages = {
   en: {
     ...enLocaleI18n,
   },
@@ -27,13 +27,15 @@ let messages = {
  * @param {*} getLocaleI18n 当前语言
  * @return {*}
  */
-let getLocaleI18n = sessionStorage.getItem('accessLocaleI18n');
+const getLocaleI18n = sessionStorage.getItem('accessLocaleI18n');
 let localeI18n = '';
-if (getLocaleI18n === null || getLocaleI18n == 'zh-CN') localeI18n = 'zh' 
-else localeI18n = 'en'
+if(getLocaleI18n) {
+  if (getLocaleI18n == 'zh-CN') localeI18n = 'zh' 
+  else localeI18n = 'en'
+}
 
-/**
- * @description: 挂载 this.$i18n.locale()
+/** 
+ * @description: 挂载this.$i18n.locale
  * @param {*} fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'zh',
  * @return {*}
  */
@@ -41,6 +43,5 @@ const i18n = new VueI18n({
   locale: localeI18n == '' ? 'zh' : localeI18n,
   messages
 })
-
 
 export default i18n
